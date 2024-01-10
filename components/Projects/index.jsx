317746303a5566a6922370b783a66ae8779272b9
seon <seon@ziten.co.kr>
2024-01-10 12:00:54 +0900

    수정

--- components/Projects/index.jsx
+++ components/Projects/index.jsx
@@ -3,15 +3,14 @@ import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import styles from "./style.module.scss";
+import styles from "./tet.module.scss";
 import { gsap } from "gsap";
 import { AnimatePresence } from "framer-motion";
 
 export default function Projects() {
   return (
     <>
-      {/* <div className={styles.projects}>프로젝트 영역</div> */}
-      <div>aaa</div>
+      <div className={styles.projects}>프로젝트 영역</div>
     </>
   );
 }
