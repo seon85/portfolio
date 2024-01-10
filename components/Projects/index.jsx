98fff7a4db28e230cad4d4fd320e6ae69df331a7
seon <seon@ziten.co.kr>
2024-01-10 11:56:44 +0900

    수정

--- components/Projects/index.jsx
+++ components/Projects/index.jsx
@@ -3,6 +3,7 @@ import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import styles from "./style.module.scss";
 import { gsap } from "gsap";
 import { AnimatePresence } from "framer-motion";
 
