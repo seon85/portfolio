addb76a83a007fc01abdd0444c43ebe33e145e8b
seon <seon@ziten.co.kr>
2024-02-05 17:13:24 +0900

    수정

--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -5,8 +5,9 @@ import styles from '../styles/sub.module.scss';
 import Link from 'next/link';
 import Image from 'next/image';
 import { createElement, useEffect, useLayoutEffect, useRef, useState } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 import Curve from '@/components/Layout/index';
-// import { slideUp } from './slideup';
 import { motion } from 'framer-motion';
 
 const slideUp = {
@@ -23,6 +24,7 @@ export default function Portfolio() {
   // const datas = require('/public/data.json');
   const portfolioList = useRef(null);
   const loader = useRef(null);
+  const portTit = useRef(null);
   let page = 0;
   let limit = 10;
   let total;
@@ -41,9 +43,10 @@ export default function Portfolio() {
             `<div class="${styles.port}">
               <a href="#" target="_blank" class="${styles.port_link}">
                 <div class="${styles.port_image}"><img src="${result.image}" alt="${result.title}"></div>
-                <div class="${styles.port_tit}">${result.title}</div>
-                <div>
-                  <div>
+                <div class="${styles.port_tit}"><h2>${result.title}</h2></div>
+                <div class="${styles.posi_date}">
+                  <div class="${styles.l}">${result.position}</div>
+                  <div class="${styles.r}">${result.date}</div>
                 </div>
               </a>
             </div>
@@ -78,7 +81,9 @@ export default function Portfolio() {
       {/* <PageLoading /> */}
       <Curve>
         <motion.div className={styles.container} variants={slideUp} initial="initial" animate="enter">
-          <h2 className={styles.subTit}>Creating next level digital products</h2>
+          <h2 className={styles.subTit} ref={portTit}>
+            Creating next level digital products
+          </h2>
           <div className={styles.portfolio_list} ref={portfolioList}></div>
         </motion.div>
         <div className={`${styles.loader} ${styles.hidden}`} ref={loader}>
