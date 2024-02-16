31d07dbefaffafca9fc996be9456ddb4d44f8d09
seon <seon@ziten.co.kr>
2024-02-16 12:55:36 +0900

    수정

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 'use client';
 import { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import '@/styles/sub.module.scss';
 import Head from 'next/head';
 // import Image from 'next/image';
 // import Link from 'next/link';
