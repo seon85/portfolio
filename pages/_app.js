54f443bb88086c9041a5b1a5ee210489ec512c8f
seon <seon@ziten.co.kr>
2024-03-25 09:31:28 +0900

    수정

--- pages/_app.js
+++ pages/_app.js
@@ -24,8 +24,7 @@ export default function App({ Component, pageProps, router }) {
     //const top = document.querySelector('body').getBoundingClientRect().top;
     //window.scrollTo(0, top);
     window.scrollTo(0, 0);
-    ScrollTrigger.getAll().forEach(t => t.kill());
-    console.log(ScrollTrigger.getAll());
+    //console.log('a');
 
     const handleRouteComplete = (url, { shallow }) => {
       setTimeout(() => {
@@ -66,6 +65,7 @@ export default function App({ Component, pageProps, router }) {
       //const top = document.querySelector('body').getBoundingClientRect().top;
       //window.scrollTo(0, top);
       window.scrollTo(0, 0);
+      ScrollTrigger.getAll().forEach(t => t.kill());
       //console.log('top');
     }, 500);
 
