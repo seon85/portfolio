babaf35f2bf8ba451c9bb4697e694fded549d235
seon <seon@ziten.co.kr>
2024-03-25 12:47:23 +0900

    수정

--- pages/_app.js
+++ pages/_app.js
@@ -57,7 +57,7 @@ export default function App({ Component, pageProps, router }) {
       const LocomotiveScroll = (await import('locomotive-scroll')).default;
       const locomotiveScroll = new LocomotiveScroll();
     })();
-    window.history.scrollRestoration = 'auto';
+
     window.scrollTo(0, 0);
 
     setTimeout(() => {
@@ -90,7 +90,7 @@ export default function App({ Component, pageProps, router }) {
     <>
       <Header />
       <main>
-        <AnimatePresence mode="wait">
+        <AnimatePresence>
           <Component key={router.route} {...pageProps} />
         </AnimatePresence>
       </main>
