b48c19f5148c2c45fd90f1a3c57737d9b89635e6
seon <seon@ziten.co.kr>
2024-03-25 14:08:39 +0900

    수정

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ export default function Home() {
         //setIsLoading(false);
         //document.body.style.cursor = 'default';
         window.scrollTo(0, 0);
-      }, 100);
+      }, 1);
     })();
     // const handleRouteChange = (url, { shallow }) => {
     //   document.body.classList.add('main_hidden');
