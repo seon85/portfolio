7734bff05300b2168f0a1119cda7f37407a823c0
seon <seon@ziten.co.kr>
2024-03-25 14:13:33 +0900

    수정

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,8 @@ import SlidingImages from '../components/SlidingImages';
 import Contact from '../components/Contact';
 import { useRouter } from 'next/router';
 import Curve from '@/components/Layout/index';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 // import { motion } from 'framer-motion';
 
 // import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -35,8 +37,13 @@ export default function Home() {
 
   const [isLoading, setIsLoading] = useState(true);
 
+  useLayoutEffect(() => {
+    ScrollTrigger.getAll().forEach(t => t.kill());
+  }, []);
+
   useEffect(() => {
     window.scrollTo(0, 0);
+
     // (async () => {
     //   setTimeout(() => {
     //     setIsLoading(false);
